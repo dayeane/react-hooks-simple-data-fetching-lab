d7ecd48231299ac5378f012be762c89d3a3bf14d
Dayeane <dayeane85@gmail.com>
2021-11-07 19:26:27 -0800

    data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+
+function App() {
+
+    useEffect( () => {
+        const img = document.querySelector("img")
+
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())   
+        .then((data) => img.src = data.message )
+
+        document.querySelector("p").innerText = ""
+        img.alt="A Random Dog"
+    });
+
+
+   
+    return (
+        <div>
+            <p>"Loading..."</p>
+            <img  src="" alt=""></img>
+        </div>
+    )
+}
+
+
+export default App;
