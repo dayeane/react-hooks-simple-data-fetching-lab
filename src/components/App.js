9c263b663d7833139cd5d773a039ebd41ebcbefa
Dayeane <dayeane85@gmail.com>
2021-11-08 17:36:02 -0800

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,19 @@ import { useEffect, useState } from "react";
 
 
 function App() {
-
+    const [loading, setLoading]= useState(null)
+    
     useEffect( () => {
-        const img = document.querySelector("img")
 
         fetch("https://dog.ceo/api/breeds/image/random")
         .then((r) => r.json())   
-        .then((data) => img.src = data.message )
-
-        document.querySelector("p").innerText = ""
-        img.alt="A Random Dog"
-    });
-
+        .then((data) => setLoading(data.message) )
+    }, []);
 
    
     return (
         <div>
-            <p>"Loading..."</p>
-            <img  src="" alt=""></img>
+            { loading ? <img  src={loading} alt="A Random Dog"></img> : <p>"Loading..."</p> }
         </div>
     )
 }
